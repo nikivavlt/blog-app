a79d835e267b60db68b3df3b02b2fedc35f3613f
nikivavlt <nikivavlt@gmail.com>
2024-06-23 11:48:34 +0300

    Create React and Express applications
    Create applications, remove unnecessary files and dependencies

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
